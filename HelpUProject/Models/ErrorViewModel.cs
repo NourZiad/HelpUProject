556b9d08f798f00a2c1c9b60937189b713e99473
DELL <DELL@DESKTOP-JJVSR3M>
2023-07-18 17:52:22 +0300

    Add project files.

---
+++ HelpUProject/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HelpUProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
